3b4632ba38531d612c5c6e681bcdae940ff6bc32
Hashim Siddiqui <hashimedt@gmail.com>
2023-01-31 22:55:57 -0600

    semiFinal

--- script.js
+++ script.js
@@ -18,24 +18,69 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   // Part 1
 
+  let title = document.querySelector('#main-title')
+
+  title.textContent = 'Welcome!'
+
 
   // Part 2
+
+  document.body.style.backgroundColor = 'red'
 
 
   // Part 3
 
+  let fav = document.getElementById("favorite-things")
+
+  fav.removeChild(fav.lastElementChild)
+
 
   // Part 4
+
+ let font2 = document.querySelectorAll('.special-title')
+
+ font2.forEach((element) => {
+  element.style.fontSize = '2rem'
+});
 
 
   // Part 5
 
 
+  let past = document.querySelector("#past-races")
+
+  past.removeChild(past.childNodes[7])
+  
   // Part 6
 
+  const li = document.createElement('li')
+  li.textContent = 'CITY'
+
+  const ul = document.querySelector("#past-races")
+    ul.appendChild(li)
 
   // Part 7
 
+  const div = document.createElement('div')
+
+  div.className = 'blog-post'
+
+  document.body.appendChild(div)
+
+  const h2 = document.createElement('h2')
+  h2.textContent = li.textContent
+
+
+  const p = document.createElement('p')
+  p.textContent = 'Lorem'
+
+  
+  div.appendChild(h2)
+  div.appendChild(p)
+
+  console.dir(div)
+  console.log(document.querySelectorAll("h1"))
+  console.log(document.querySelectorAll("h2"))
 
   // Part 8
 
